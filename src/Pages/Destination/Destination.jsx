3accc5bbe842a6eb470d3c9b93c521b6225d7dd7
Divya Sharma <divyasharma007d@gmail.com>
2025-06-08 23:37:17 +0530

    Fix Navbar import path to resolve Netlify build error

--- src/Pages/Destination/Destination.jsx
+++ src/Pages/Destination/Destination.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from "../../Components/Navbar/Navbar";
-import Footer from "../components/Footer";
-import First from "../../components/Destination/First";
-import Firstcards from '../../components/Destination/Firstcards'
-import Secondimg from '../../components/Destination/Secondimg'
-import Third from '../../components/Destination/Third'
-import Fourth from '../../components/Destination/Fourth'
+import Footer from "../../Components/Footer";
+import First from "../../Components/Destination/First";
+import Firstcards from '../../Components/Destination/Firstcards'
+import Secondimg from '../../Components/Destination/Secondimg'
+import Third from '../../Components/Destination/Third'
+import Fourth from '../../Components/Destination/Fourth'
 import "./destination.css";
 
 const Destination = () => {
