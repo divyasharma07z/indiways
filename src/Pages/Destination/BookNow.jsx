55ed09e481031945f82f5fbf8e702ddbb9d7cd17
Divya Sharma <divyasharma007d@gmail.com>
2025-06-08 23:25:51 +0530

    Update BookNow.jsx with necessary changes

--- src/Pages/Destination/BookNow.jsx
+++ src/Pages/Destination/BookNow.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./BookNow.css";
-import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer";
-
+import Navbar from "../../components/Navbar";
+import Footer from "../components/Footer";
 const BookNow = () => {
   const location = useLocation();
   const navigate = useNavigate();
